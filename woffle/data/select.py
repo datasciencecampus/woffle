5880771b5afcb0b591cc83345f4938f9f4bf554a
karetsu <myogibo@gmail.com>
2018-12-10 21:22:41 +0000

    feat: updated select with expected behaviour
    functions are currently placeholders so they really need to be written
    but this is a step toward how I want it to work

--- woffle/data/select.py
+++ woffle/data/select.py
@@ -1,29 +1,107 @@
-# an idea of how this should work
-# +TODO: write these two, they are super simple
-# from woffle.functions.lists import head, tail
-head = lambda l: l[0]
-tail = lambda l: l[1:]
+"""
+Selection of the label for the cluster based on decision metric
+"""
+# -- Imports --------------------------------------------------------------------
+# base
+import itertools
 
+from typing import List
+
+
+# third party
+from textacy.similarity import levenshtein
+
+
+# project
+from woffle.functions.id import id
+
+
+# -- Type synonyms --------------------------------------------------------------
+# +TODO: figure out of we need any...
+
+
+# -- Definitions ----------------------------------------------------------------
+
+# -- Decision functions -- #
+
+# maps levenshtein distance into the closed interval [0,1]
+def edCond(xs: List[str]) -> float:
+    "aver1age edit distance on cluster"
+    return (
+        0
+        if len(xs) == 1
+        else sum([levenshtein(*ys) for ys in itertools.combinations(xs, 2)]) / len(xs)
+    )
+
+
+# wordgram measurement
+def wgCond(xs: List[str]) -> float:
+    "average number of common words across the cluster"
+    return 0
+
+
+# charactergram measurement
+def ngCond(xs: List[str]) -> float:
+    "longest common ngram"
+    return 0
+
+
+# +TODO: decide if there is a better than wordnet approach with spacy
+# semantic similarity
+def hnCond(xs: List[str]) -> float:
+    "implement hypernym lookup"
+    return 0
+
+
+# -- Text replacement functions -- #
+# +TODO: write these functions
+
+
+def edit(xs: List[str]) -> str:
+    return "Yep, I need to implement edit distance selector"
+
+
+def wordgram(xs: List[str]) -> str:
+    return "Yep, I need to implement wordgram selector"
+
+
+def chargram(xs: List[str]) -> str:
+    return "Yep, I need to implement character-gram selector"
+
+
+def hypernyms(xs: List[str]) -> str:
+    return "Yep, I need to implement hypernym selector"
+
+
+def fallback(xs: List[str]) -> str:
+    return "Yep, I need to implement the fallback selector"
+
+
+# -- Exports --------------------------------------------------------------------
+# +TODO: replace all these 0.75s with proper thresholds
 
 # given conditions on the cluster and a function to run in each condition then
 # do the following in a more abstract way
-conditions = [lambda x: 0, lambda x: 0, lambda x: 0, lambda x: 0]
-functions = [lambda x: x, lambda x: x, lambda x: x, lambda x: x]
-
-zipped = zip(conditions, functions)
-
-
-def decide(fs, x):
-    return [i(x) and j(x) for i, j in fs]
-
-
-def select(cluster):
-    return head(cluster) or select(tail(cluster))
+decisions = (
+    lambda xs: edCond(xs) > 0.75,  # high lexical similarity
+    lambda xs: wgCond(xs) > 0.75,  # medium lexical similarity
+    lambda xs: ngCond(xs) > 0.75,  # low lexical similarity
+    lambda xs: hnCond(xs) > 0.75,  # semantic similarity
+    lambda xs: id(xs) == xs,  # default always true fallback
+)
 
 
-# this code will not work, but the plan here then is that we can just use a
-# combination of decide and select to combine the functions on the values
-# in the way we want - its the most obvious way to get what we want
-#
-# decide is also horrific in terms of computation, we will turn this into a
-# generator function so as to only process the decision as far as is needed
+functions = (
+    lambda xs: edit(xs),  # high lexical similarity
+    lambda xs: wordgram(xs),  # medium lexical similarity
+    lambda xs: chargram(xs),  # low lexical similarity
+    lambda xs: hypernyms(xs),  # semantic similarity
+    lambda xs: fallback(xs),  # default fallback
+)
+
+
+# exposed default interface
+def select(decisions, functions, cluster):
+    for d, f in zip(decisions, functions):
+        if d(cluster):
+            return f(cluster)  # stops the first time that d(cluster) is true
