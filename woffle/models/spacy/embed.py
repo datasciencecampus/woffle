adae8148f52f87114fbc53c664bc37560e91dbf5
Finnkauski <iwiivi@gmail.com>
2018-12-12 16:24:37 +0000

    Develop (#14)
    * feature: spacy support. Note: Issue with reliability of typechecking
    
    * tweak: added Pipfiles to .gitignore

--- woffle/models/spacy/embed.py
+++ woffle/models/spacy/embed.py
@@ -6,19 +6,32 @@ embed :: String -> [Float], everything else should just be part of it
 """
 
 #-- Imports ---------------------------------------------------------------------
+# base 
+import functools 
+
+from typing import List, NewType
 # third party
 import spacy
 import toml
 
 # project
 from woffle.functions.compose import compose
+from woffle.functions.id      import id
 
+
+#-- Type synonyms ---------------------------------------------------------------
+# TODO: Pyre doesn't really pick up or enforce this type signiture unfortunately. The type for the model is spacy.lang.en.English (for en_core_web_md)
+Model = NewType('Model', int)
 
 #-- Definitions -----------------------------------------------------------------
+
+# variables
 config = toml.load('config.ini')
-# load the spacy model
-proc = spacy.load(config['spacy']['model'])
+model = spacy.load(config['spacy']['model'])
 
 
+# functions
 
-embed = lambda x: x
+def embedding(m: Model, x : str) -> List[float]:
+    return m(x).vector.tolist()
+embed = functools.partial(embedding, model)
