5e7e95aa2bc062091941d86f9697d44d352673ed
karetsu <myogibo@gmail.com>
2018-12-12 19:20:41 +0000

    refactor: autolinter did its job

--- woffle/models/spacy/embed.py
+++ woffle/models/spacy/embed.py
@@ -5,33 +5,39 @@ The important function here is 'embed' which has signature
 embed :: String -> [Float], everything else should just be part of it
 """
 
-#-- Imports ---------------------------------------------------------------------
-# base 
-import functools 
+# -- Imports ---------------------------------------------------------------------
+# base
+import functools
 
 from typing import List, NewType
+
+
 # third party
 import spacy
 import toml
 
+
 # project
 from woffle.functions.compose import compose
-from woffle.functions.id      import id
+from woffle.functions.id import id
 
 
-#-- Type synonyms ---------------------------------------------------------------
-# TODO: Pyre doesn't really pick up or enforce this type signiture unfortunately. The type for the model is spacy.lang.en.English (for en_core_web_md)
-Model = NewType('Model', int)
+# -- Type synonyms ---------------------------------------------------------------
+# TODO: Pyre doesn't really pick up or enforce this type signiture unfortunately.
+# The type for the model is spacy.lang.en.English (for en_core_web_md)
+Model = NewType("Model", int)
 
-#-- Definitions -----------------------------------------------------------------
+
+# -- Definitions -----------------------------------------------------------------
 
 # variables
-config = toml.load('config.ini')
-model = spacy.load(config['spacy']['model'])
+config = toml.load("config.ini")
+model = spacy.load(config["spacy"]["model"])
 
 
 # functions
-
-def embedding(m: Model, x : str) -> List[float]:
+def embedding(m: Model, x: str) -> List[float]:
     return m(x).vector.tolist()
+
+
 embed = functools.partial(embedding, model)
