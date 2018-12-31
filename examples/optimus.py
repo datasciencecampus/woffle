ee58c90214a7b576c83623577fd7acf48aa9d71c
karetsu <myogibo@gmail.com>
2018-12-31 17:43:55 +0000

    examples: tidying/refactor of optimus

--- examples/optimus.py
+++ examples/optimus.py
@@ -27,30 +27,31 @@ def main():
     depth = 1
     labels = {}
 
+    #+TODO: consider immutable version of this but its perhaps not really needed
+    # mutable list to update the target words at each depth
+    targetM = target
+
     while numclusters > 1:
+
         print(f"** Depth {depth}")
         print("    -- embedding")
-        embedding = list(embed(target))  ## TODO: clusters cannot currently take a map
+        embedding = list(embed(targetM))  ## TODO: clusters cannot currently take a map
 
         print(f"    -- clustering")
-        clusters = cluster(embedding, target, depth)
+        clusters = cluster(embedding, targetM, depth)
 
         if len(set(clusters)) == numclusters:
             print("    >> No new clusters generated")
             labels[f"tier_{depth}"] = labels[f"tier_{depth-1}"]
-            depth += 1
         else:
             numclusters = len(set(clusters))
 
             print("    -- generating labels")
-            labels[f"tier_{depth}"] = list(select(target, clusters))
+            labels[f"tier_{depth}"] = list(select(targetM, clusters))
+            #+TODO: make this better
+            targetM = [i if i else j for i,j in zip(labels[f"tier_{depth}"], targetM)]
 
-            target = [ labels[f"tier_{depth}"][i]
-                      if labels[f"tier_{depth}"][i]
-                      else text[i]
-                      for i in range(len(text))
-                     ]
-            depth += 1
+        depth += 1
 
 
 # -- Boilerplate ----------------------------------------------------------------
