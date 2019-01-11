d989f7e6818a65b436bcf52e78f2b2fe2172cb7d
finnkauski <iwiivi@gmail.com>
2019-01-11 13:52:41 +0000

    Wordgram scoring (#35)
    * feat: added example sentiment scoring
    
    * refactor: changed the imports to avoid long namespaces, added more example sentences
    
    * feat: reworked wordgram scorer to mirror optimus

--- woffle/select/lexical/wordgrams.py
+++ woffle/select/lexical/wordgrams.py
@@ -6,7 +6,10 @@ wordgram decision metrics for lexical similarity of text
 # base
 import itertools
 
+from collections import Counter
 from typing import List, NewType, Set
+from math import log
+from operator import add
 
 # third party
 import numpy as np
@@ -14,8 +17,7 @@ import numpy as np
 from textacy.similarity import jaccard
 
 # project
-from woffle.functions.lists import strip
-
+from woffle.functions.lists import strip, foldl
 
 # -- Type synonyms --------------------------------------------------------------
 # +TODO: figure out of we need any...
@@ -48,4 +50,35 @@ def fetch(x: str, n: int) -> Set[str]:
 
 
 def group(x:str) -> Set[str]:
-    return set.union(*(fetch(x, i) for i in range(1, len(x.split())+1)))
+    return (x and set.union(*(fetch(x, i) for i in range(1, len(x.split())+1)))) or set()
+
+
+def max_len(xs: List[str]) -> int:
+    tokens = map(str.split, xs)
+    return max([len(w) for t in tokens for w in t])
+
+
+def scorer(xs: List[str]) -> float:
+   count    = [Counter(group(s)) for s in xs]
+   counts   = foldl(add, Counter(), count)
+   score    = lambda d: {i:(d[i]**2 * (1+log(len(i.split())))) for i in d}
+   scores   = score(counts)
+   r        = max(scores, key=lambda key: scores[key])
+   return r, scores[r]
+
+#+TODO: this is an over-complicated mechanism when a list of scores would work
+
+# -- Interfaces -----------------------------------------------------------------
+# wordgram measurement
+def condition(xs: List[str]) -> float:
+    "average number of common words across the cluster"
+    xs_ = strip(xs)
+    return (
+        0.0 if (len(xs_) <= 1) or (max_len(xs_) < 3) else
+        scorer(xs_)[1]
+    )
+
+
+def selection(xs: List[str]) -> str:
+    xs_ = strip(xs)
+    return scorer(xs_)[0]
