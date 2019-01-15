fa2f62061ee385fc1e194a904ac8683594de9f6e
finnkauski <iwiivi@gmail.com>
2019-01-15 10:05:56 +0000

    Chargrams (#36)
    * feat: added example sentiment scoring
    
    * refactor: changed the imports to avoid long namespaces, added more example sentences
    
    * feat: reworked chargram, needs refactoring to align with worgrams
    
    * refactor: minor changes to code in chargrams
    
    * refactor: removed getMatch from chargrams function list
    
    * todos: added some todos to note that the levenhstein might be different

--- woffle/select/lexical/chargrams.py
+++ woffle/select/lexical/chargrams.py
@@ -6,40 +6,49 @@ wordgram decision metrics for lexical similarity of text
 # base
 import itertools
 
-from typing import List, NewType
-
+from math import log
+from typing import List, NewType, Tuple
+from difflib import SequenceMatcher
+from collections import Counter
 # third party
 import numpy as np
 
-from textacy.similarity import jaccard
+from textacy.similarity import jaccard, levenshtein 
 
 # project
-from woffle.functions.lists import strip, foldl1
+from woffle.functions.lists import strip, foldl1, unpack
 
 
 # -- Type synonyms --------------------------------------------------------------
 # +TODO: figure out of we need any...
 Array = NewType('Array', np.array)
 
+# -- Supporting functions -------------------------------------------------------
+def characters(wl: List[str], start: int = 3, finish: int = 20):
+    for word in wl:
+        for n in range(start,finish):
+            yield [word[i:i+n] for i in range(len(word) - n + 1)]
+
+def scorer(xs: List[str]) -> Tuple[str,float]:
+   counts   = Counter(unpack(characters(xs)))
+   scores   = {i:(counts[i]**2 * (1+log(len(i)))) for i in counts}
+   r        = max(scores, key=scores.get)
+   return r, scores[r]
 
 # -- Interfaces -----------------------------------------------------------------
 # charactergram measurement
 def condition(xs: List[str]) -> float:
     "longest common ngram"
     xs_ = strip(xs)
+    #TODO: Important! The formula needs rejigging as the levenshtein isnt edit_distance
+    #NOTE: look into the maths behind it
+    denom = np.mean([levenshtein(*ys) for ys in itertools.combinations(xs_, 2)])
     return (
-        0.0 #TODO: if there is only one item then its vacuously 0?
-        if len(xs) <= 1
-        else np.mean([jaccard(*ys) for ys in itertools.combinations(xs_, 2)])
+        0.0 if len(xs) <= 1 #TODO: if there is only one item then its vacuously 0? 
+        else scorer(xs_)[1]/(denom*log(len(xs)))
     )
-
 
 def selection(xs: List[str]) -> str:
     xs_ = strip(xs)
-    return foldl1(getMatch, xs_)
+    return scorer(xs_)[0]
 
-
-# -- Supporting functions -------------------------------------------------------
-def getMatch(x: str, y: str) -> str:
-    match = SequenceMatcher(None, x, y).find_longest_match(0, len(x), 0, len(y))
-    return x[match.a : match.a + match.size]
