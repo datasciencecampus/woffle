2b7eb1c47ceb82b0cbb31480f1961276caa2134d
karetsu <myogibo@gmail.com>
2019-01-28 12:18:22 +0000

    bug: updated chargrams to not break on short lists

--- woffle/select/lexical/chargrams.py
+++ woffle/select/lexical/chargrams.py
@@ -5,6 +5,7 @@ wordgram decision metrics for lexical similarity of text
 # -- Imports --------------------------------------------------------------------
 # base
 import itertools
+import re
 
 from math import log
 from typing import List, NewType, Tuple
@@ -23,39 +24,53 @@ from woffle.functions.lists import strip, foldl1, unpack
 # +TODO: figure out of we need any...
 Array = NewType('Array', np.array)
 
-# -- Supporting functions -------------------------------------------------------
-def edits(s1: str,s2: str) -> float:
-    normalised = levenshtein(s1, s2)
-    maximum    = max(map(len,(s1,s2)))
-    return maximum*(1 - normalised)
-
-def characters(wl: List[str], start: int = 3, finish: int = 20):
-    for word in wl:
-        for n in range(start,finish):
-            yield [word[i:i+n] for i in range(len(word) - n + 1)]
-
-def scorer(xs: List[str]) -> Tuple[str,float]:
-   counts   = Counter(unpack(characters(xs)))
-   scores   = {i:(counts[i]**2 * (1+log(len(i)))) for i in counts}
-   r        = max(scores, key=scores.get)
-   return r, scores[r]
 
 # -- Interfaces -----------------------------------------------------------------
 # charactergram measurement
 def condition(xs: List[str]) -> float:
     "longest common ngram"
     xs_ = strip(xs)
-    # +TODO: will require rethinking the sizes of thresholds
-    if len(xs_) == 1:
-        denom=0
-    else:
-        denom = np.mean([edits(*ys) for ys in itertools.combinations(xs_, 2)])
     return (
-        0.0 if len(xs) <= 1 #TODO: if there is only one item then its vacuously 0?
-        else scorer(xs_)[1]/(1+denom*log(len(xs)))
+        0.0 if ((len(xs_) <= 1) or (np.min([len(x) for x in xs_]) < 3))
+        #TODO: if there is only one item then its vacuously 0?
+        else scorer(xs_)
     )
+
 
 def selection(xs: List[str]) -> str:
     xs_ = strip(xs)
-    return scorer(xs_)[0]
+    return label(xs_)
 
+
+# -- Supporting functions -------------------------------------------------------
+def edits(s1: str,s2: str) -> float:
+    normalised = levenshtein(s1, s2)
+    maximum    = max(map(len,(s1,s2)))
+    return maximum*(1 - normalised)
+
+def characters(xs: List[str], start: int = 3, finish: int = 20):
+    return unpack([get(x, n) for x in xs for n in range(start, finish)])
+
+
+def get(x: str, n: int) -> List[str]:
+    # fetch all charactergrams of length n from x
+    regex = f"(?=({'.'*n}))"
+    return re.findall(regex, x)
+
+# alternative (bad) version
+get_ = lambda n: lambda x: re.findall(f"(?=({'.'*n}))", x)
+
+
+def scorer(xs: List[str]) -> float:
+    cgrams = characters(xs)
+    score = (sum(i in j for j in xs)**2 * (1+log(len(i))) for i in cgrams)
+    return max(score)
+
+
+def label(xs: List[str]) -> float:
+    cgrams = characters(xs)
+    score = [np.sum(i in j for j in xs)**2 * (1+log(len(i))) for i in cgrams]
+    candidate = [cgrams[i] for i,j in enumerate(score) if j==np.max(score)]
+    return candidate[0]
+
+#+TODO: brain not working, make this betterer
